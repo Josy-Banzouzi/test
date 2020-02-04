98a646529e01ad0b21271cfefd19f7da79a46e6d
Josy <josymisere@gmail.com>
2020-02-05 00:13:26 +0100

    2eme cas b est paire et calcul de l'equation tout en verifiant tout les cas du discrimant

--- equation.py
+++ equation.py
@@ -34,4 +34,24 @@ class Equation:
                   cls.X3 = (- cls.nombre_2)/(2 * cls.nombre_1)
                   print("l'equation admet une racine double X3 ==  {}".format(cls.X3))
 
+         elif cls.nombre_2 % 2 == 0:
+              print("le nombre_2 est : paire ")
+
+              cls.X4 = cls.nombre_2/2
+              cls.discriminant = ((cls.X4 * cls.X4) -  cls.nombre_1 * cls.nombre_3)
+
+              if cls.discriminant < 0:
+                 print("l'equation n'admet aucune racine il n'y a pas de solution")
+              elif cls.discriminant > 0:
+
+                   cls.X1 = ((-cls.X4) - 2 * math.sqrt(cls.discriminant))/(cls.nombre_1)
+
+                   cls.X2 = ((-cls.X4) + 2 * math.sqrt(cls.discriminant))/(cls.nombre_1)
+
+                    print("l'equation admet deux racine distinct X1 == {} et X1 == {} ".format(cls.X1 , cls.X2))
+
+              else:
+                  cls.X3 = (- cls.X4)/( cls.nombre_1)
+                  print("l'equation admet une racine double X3 ==  {}".format(cls.X3))
+
 Equation.calcul_fonction()
