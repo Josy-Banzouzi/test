0092307f3074ad7b62ddd8fbe9e464317d4e5eb4
Josy <josymisere@gmail.com>
2020-02-05 00:08:56 +0100

    pour un b impaire et calcul de l'equation pour tout les cas du discriminant

--- equation.py
+++ equation.py
@@ -19,4 +19,19 @@ class Equation:
               print("le nombre_2 est : impaire ")
               cls.discriminant = ((cls.nombre_2 * cls.nombre_2) - 4 * cls.nombre_1 * cls.nombre_3)
 
+              if cls.discriminant < 0:
+
+                  print("l'equation n'admet aucune racine il n'y a pas de solution")
+              elif cls.discriminant > 0:
+
+                  cls.X1 = ((-cls.nombre_2) - 2 * math.sqrt(cls.discriminant))/(2 * cls.nombre_1)
+
+                  cls.X2 = ((-cls.nombre_2) + 2 * math.sqrt(cls.discriminant))/(2 * cls.nombre_1)
+
+                  print("l'equation admet deux racine distinct X1 == {} et X1 == {} ".format(cls.X1 , cls.X2))
+
+              else:
+                  cls.X3 = (- cls.nombre_2)/(2 * cls.nombre_1)
+                  print("l'equation admet une racine double X3 ==  {}".format(cls.X3))
+
 Equation.calcul_fonction()
